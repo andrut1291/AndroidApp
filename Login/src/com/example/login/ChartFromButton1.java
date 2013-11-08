f0a5edf5daba7952d964b2d77a09ae86faab6e9f
andrut1291 <maciekandruszko@wp.pl>
2013-11-08 20:03:41 +0100

    dzialahacy wykres multitocuh ale zwykly

--- Login/src/com/example/login/ChartFromButton1.java
+++ Login/src/com/example/login/ChartFromButton1.java
@@ -1,138 +1,71 @@
-/*
- * Copyright 2012 AndroidPlot.com
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 package com.example.login;
-
-
-import android.app.Activity;
-import android.graphics.*;
-import android.os.Bundle;
-import com.androidplot.xy.SimpleXYSeries;
-import com.androidplot.xy.XYSeries;
-import com.androidplot.xy.*;
-
-import java.text.DecimalFormat;
-import java.text.FieldPosition;
-import java.text.Format;
-import java.text.ParsePosition;
 import java.util.Arrays;
 
+
+import com.androidplot.xy.*;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+
+/**
+*
+* This example is based on:
+* - multitouch example, by David Buezas (david.buezas at gmail.com) and Michael
+* from http://androidplot.com/wiki/A_Simple_XYPlot_with_multi-touch_zooming_and_scrolling
+* - AndroidPlot quickstart example http://androidplot.com/wiki/Quickstart
+* No license was given with this samples, but I assume that use it for free on BSD-like license ;-)
+*
+* @author Marcin Lepicki (marcin.lepicki at flex-it.pl)
+*
+*/
 public class ChartFromButton1 extends Activity
 {
 
-    private XYPlot mySimpleXYPlot;
+@Override
+public void onCreate(Bundle savedInstanceState)
+{
+super.onCreate(savedInstanceState);
+setContentView(R.layout.chart_button1);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+MultitouchPlot multitouchPlot = (MultitouchPlot) findViewById(R.id.stepChartExamplePlot);
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.chart_button1);
+// Create two arrays of y-values to plot:
+Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
+Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
 
-        // initialize our XYPlot reference:
-        mySimpleXYPlot = (XYPlot) findViewById(R.id.stepChartExamplePlot);
+// Turn the above arrays into XYSeries:
+XYSeries series1 = new SimpleXYSeries(
+Arrays.asList(series1Numbers), // SimpleXYSeries takes a List so turn our array into a List
+SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
+"Obwód brzucha"); // Set the display title of the series
 
-        // y-vals to plot:
-        Number[] series1Numbers = {1,2,1,1,1,1,1,2,2,2,2,1,1,1,2,2,1};
-        // create our series from our array of nums:
-        XYSeries series2 = new SimpleXYSeries(
-                Arrays.asList(series1Numbers),
-                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,
-                "Wartoœæ napiêcia");
+// Same as above, for series2
+XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,
+"Series2");
 
-        //mySimpleXYPlot.getLayoutManager().remove(mySimpleXYPlot.getLegendWidget());
+// Create a formatter to use for drawing a series using LineAndPointRenderer:
+LineAndPointFormatter series1Format = new LineAndPointFormatter(
+Color.rgb(0, 200, 0), // line color
+Color.rgb(0, 100, 0), // point color
+Color.rgb(150, 190, 150),null); // fill color (optional)
 
+// Add series1 to the xyplot:
+multitouchPlot.addSeries(series1, series1Format);
 
-        mySimpleXYPlot.getGraphWidget().getGridBackgroundPaint().setColor(Color.WHITE);
-        mySimpleXYPlot.getGraphWidget().getDomainGridLinePaint().setColor(Color.BLACK);
-        mySimpleXYPlot.getGraphWidget().getDomainGridLinePaint().setPathEffect(new DashPathEffect(new float[]{1, 1}, 1));
-        mySimpleXYPlot.getGraphWidget().getRangeGridLinePaint().setColor(Color.BLACK);
-        mySimpleXYPlot.getGraphWidget().getRangeGridLinePaint().setPathEffect(new DashPathEffect(new float[]{1, 1}, 1));
-        mySimpleXYPlot.getGraphWidget().getDomainOriginLinePaint().setColor(Color.BLACK);
-        mySimpleXYPlot.getGraphWidget().getRangeOriginLinePaint().setColor(Color.BLACK);
-        mySimpleXYPlot.getGraphWidget().setMarginRight(5);
+// Same as above, with series2:
+multitouchPlot.addSeries(series2, new LineAndPointFormatter(Color.rgb(0, 0, 200), Color.rgb(0, 0, 100),
+Color.rgb(150, 150, 190),null));
 
-        // Create a formatter to use for drawing a series using LineAndPointRenderer:
-        LineAndPointFormatter series1Format = new LineAndPointFormatter(
-                Color.TRANSPARENT,                   // line color
-                Color.BLACK,                   // point color
-                Color.TRANSPARENT, null);                // fill color
+// Reduce the number of range labels
+multitouchPlot.setTicksPerRangeLabel(3);
 
+// By default, AndroidPlot displays developer guides to aid in laying out your plot.
+// To get rid of them call disableAllMarkup():
+//multitouchPlot.disableAllMarkup();
 
-        // setup our line fill paint to be a slightly transparent gradient:
-        Paint lineFill = new Paint();
-        lineFill.setAlpha(200);
-        lineFill.setShader(new LinearGradient(0, 0, 0, 250, Color.WHITE, Color.BLUE, Shader.TileMode.MIRROR));
+multitouchPlot.setRangeBoundaries(0, 10, BoundaryMode.FIXED);
+multitouchPlot.setDomainBoundaries(0, 2.2, BoundaryMode.FIXED);
 
-        StepFormatter stepFormatter  = new StepFormatter(Color.rgb(0, 0,0), Color.BLUE);
-        stepFormatter.getLinePaint().setStrokeWidth(1);
-
-        stepFormatter.getLinePaint().setAntiAlias(false);
-        stepFormatter.setFillPaint(lineFill);
-        mySimpleXYPlot.addSeries(series2, stepFormatter);
-
-        // adjust the domain/range ticks to make more sense; label per tick for range and label per 5 ticks domain:
-        mySimpleXYPlot.setRangeStep(XYStepMode.INCREMENT_BY_VAL, 1);
-        mySimpleXYPlot.setDomainStep(XYStepMode.INCREMENT_BY_VAL, 1);
-        mySimpleXYPlot.setTicksPerRangeLabel(1);
-        mySimpleXYPlot.setTicksPerDomainLabel(5);
-        mySimpleXYPlot.setRangeBoundaries(1, BoundaryMode.FIXED, 3, BoundaryMode.FIXED);
-        mySimpleXYPlot.getGraphWidget().setMarginTop(10);
-        // customize our domain/range labels
-        //mySimpleXYPlot.setDomainLabel("Time (Secs)");
-        //mySimpleXYPlot.setRangeLabel("Server State");
-
-       //mySimpleXYPlot.getGraphWidget().getDomainGridLinePaint().setAlpha(0);
-      
-
-
-        // get rid of decimal points in our domain labels:
-        mySimpleXYPlot.setDomainValueFormat(new DecimalFormat("0"));
-        //mySimpleXYPlot.getGraphWidget().setDrawMarkersEnabled(false);
-        // create a custom formatter to draw our state names as range tick labels:
-        mySimpleXYPlot.setRangeValueFormat(new Format() {
-            @Override
-            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
-                Number num = (Number) obj;
-                switch(num.intValue()) {
-                    case 1:
-                        toAppendTo.append("0 [V]");
-                        break;
-                    case 2:
-                        toAppendTo.append("12 [V]");
-                        break;
-                    case 3:
-                    	toAppendTo.append("24 [V]");
-                    	break;
-                    default:
-                        toAppendTo.append("Unknown");
-                        break;
-                }
-                return toAppendTo;
-            }
-
-            @Override
-            public Object parseObject(String source, ParsePosition pos) {
-                return null;
-            }
-        });
-
-        // by default, AndroidPlot displays developer guides to aid in laying out your plot.
-        // To get rid of them call disableAllMarkup():
-        //mySimpleXYPlot.disableAllMarkup();
-
-    }
+}
 }
